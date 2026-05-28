b7a892a45c56394e5eac7677d3c6351880a304f2
Baez <mail@mail.com>
2026-05-28 10:56:02 -0600

    test(arch): enforce core/disciplines boundaries and Vogen ID usage via NetArchTest

---
+++ tests/Sport.Architecture.Tests/ArchitectureRules.cs
@@ -0,0 +1,79 @@
+using FluentAssertions;
+using NetArchTest.Rules;
+using Sport.Core.DisciplineRegistry;
+
+namespace Sport.Architecture.Tests;
+
+public class ArchitectureRules
+{
+    private static readonly System.Reflection.Assembly CoreAssembly = typeof(IDisciplineModule).Assembly;
+
+    private static readonly System.Reflection.Assembly[] DisciplineAssemblies =
+    {
+        typeof(Sport.Disciplines.FBL.FblModule).Assembly,
+        typeof(Sport.Disciplines.BKB.BkbModule).Assembly,
+        typeof(Sport.Disciplines.BDM.BdmModule).Assembly,
+        typeof(Sport.Disciplines.VBV.VbvModule).Assembly,
+        typeof(Sport.Disciplines.BOX.BoxModule).Assembly,
+        typeof(Sport.Disciplines.ATH.AthModule).Assembly,
+    };
+
+    [Fact]
+    public void Core_does_not_reference_any_discipline_module()
+    {
+        foreach (var disciplineAsm in DisciplineAssemblies)
+        {
+            var disciplineNs = disciplineAsm.GetName().Name!;
+            var result = Types.InAssembly(CoreAssembly)
+                .Should()
+                .NotHaveDependencyOn(disciplineNs)
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue($"Sport.Core must not depend on {disciplineNs}");
+        }
+    }
+
+    [Fact]
+    public void Each_discipline_does_not_reference_another_discipline()
+    {
+        foreach (var asm in DisciplineAssemblies)
+        {
+            var selfNs = asm.GetName().Name!;
+            foreach (var other in DisciplineAssemblies)
+            {
+                var otherNs = other.GetName().Name!;
+                if (otherNs == selfNs) continue;
+
+                var result = Types.InAssembly(asm)
+                    .Should()
+                    .NotHaveDependencyOn(otherNs)
+                    .GetResult();
+
+                result.IsSuccessful.Should().BeTrue($"{selfNs} must not depend on {otherNs}");
+            }
+        }
+    }
+
+    [Fact]
+    public void All_aggregate_root_IDs_use_Vogen_value_objects_not_raw_Guid()
+    {
+        var rootClassNames = new[]
+        {
+            "Competition", "CompetitionDiscipline",
+            "Event", "Phase", "Unit", "Subunit",
+            "Person", "Organisation", "Team", "Entry",
+            "OfficialAssignment",
+        };
+
+        foreach (var name in rootClassNames)
+        {
+            var type = CoreAssembly.GetTypes().SingleOrDefault(t => t.Name == name);
+            type.Should().NotBeNull($"aggregate root '{name}' must exist in Sport.Core");
+
+            var idProp = type!.GetProperty("Id");
+            idProp.Should().NotBeNull($"'{name}.Id' must be defined");
+            idProp!.PropertyType.Should().NotBe(typeof(Guid),
+                $"'{name}.Id' must be a Vogen typed ID, not raw Guid");
+        }
+    }
+}
